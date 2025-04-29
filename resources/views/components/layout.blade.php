aa2db6edd7e0efc017e803a82dd0f6af9f961ed5
Syazwan <28nursyazwan@gmail.com>
2025-04-30 00:11:05 +0800

    Add JobController and job listing view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,14 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel Job Board</title>
         @vite('resources/css/app.css')
-
-
     </head>
+
     <body >
-        <div>
-             <h1 class="text-6xl font-bold underline">
-                Welcome to the Laravel Job Board
-            </h1>
-        </div>
+        {{ $slot }}
     </body>
 </html>
