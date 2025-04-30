9b2c5276a186f8e791a00841782ffd1b3e53b596
syazwan <28nursyazwan@gmail.com>
2025-04-30 19:29:49 +0800

    Refactor job listing layout and add card component for better presentation

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,8 +1,16 @@
 <x-layout>
     @foreach ($jobs as $job )
-        <div>
-            {{ $job->title }}
-        </div>
+        <x-card class="m-4">
+            <div class="flex justify-between mb-4">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-slate-500">
+                    RM{{ number_format($job->salary)  }}
+                </div>
+            </div>
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($job->description)) !!}
+            </p>
+        </x-card>
 
     @endforeach
 </x-layout>
