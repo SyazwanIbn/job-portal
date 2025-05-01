e2f4323fc97f07ce76b71305da8c658b0939028e
Syazwan <28nursyazwan@gmail.com>
2025-05-01 22:27:38 +0800

    Add job tags and company name to job listing view

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 
+Route::get('', function () {
+    return to_route('jobs.index');
+});
 
 Route::resource('jobs', JobController::class)
     ->only(['index']);
