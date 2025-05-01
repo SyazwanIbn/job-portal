1bee6b65b436071250c146866599b59bdfb0c4c5
Syazwan <28nursyazwan@gmail.com>
2025-05-01 22:38:12 +0800

    Fix layout issues in job listing view for better alignment of company name and tags

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -8,13 +8,13 @@
                 </div>
             </div>
 
-            <div class="mb-4 flex justify-between text-sm text-slate-500">
+            <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
                 <div class="flex space-x-2">
                     <div>Company Name:</div>
                     <div>{{ $job->location  }}</div>
                 </div>
 
-                <div class="flex item-center space-x-2 text-xs">
+                <div class="flex items-center space-x-2 text-xs">
                     <x-tag class="rounded-md border px-2 py-1">{{ Str::ucfirst($job->experience) }}</x-tag>
                     <x-tag class="rounded-md border px-2 py-1">{{ $job->category }}</x-tag>
                 </div>
