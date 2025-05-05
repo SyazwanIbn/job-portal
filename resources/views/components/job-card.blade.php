e6900516fd432099bf4aa7015c7b8b52e803c437
syazwan <28nursyazwan@gmail.com>
2025-05-05 18:12:19 +0800

    Implement job detail view and refactor job listing to use job card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,27 @@
+<x-card class="m-4">
+    <div class="flex justify-between mb-4">
+        <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+        <div class="text-slate-500">
+            RM{{ number_format($job->salary)  }}
+        </div>
+    </div>
+
+    <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
+        <div class="flex space-x-2">
+            <div>Company Name:</div>
+            <div>{{ $job->location  }}</div>
+        </div>
+
+        <div class="flex items-center space-x-2 text-xs">
+            <x-tag class="rounded-md border px-2 py-1">{{ Str::ucfirst($job->experience) }}</x-tag>
+            <x-tag class="rounded-md border px-2 py-1">{{ $job->category }}</x-tag>
+        </div>
+    </div>
+
+    <p class="text-sm text-slate-500 mb-4">
+        {!! nl2br(e($job->description)) !!}
+
+    </p>
+
+    {{  $slot }}
+</x-card>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,30 +1,15 @@
 <x-layout>
     @foreach ($jobs as $job )
-        <x-card class="m-4">
-            <div class="flex justify-between mb-4">
-                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
-                <div class="text-slate-500">
-                    RM{{ number_format($job->salary)  }}
-                </div>
+        <x-job-card class="m-4" :$job>
+            <div>
+                <x-link-button : href="route('jobs.show', $job)">
+                    Show
+                </x-link-button>
             </div>
-
-            <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
-                <div class="flex space-x-2">
-                    <div>Company Name:</div>
-                    <div>{{ $job->location  }}</div>
-                </div>
-
-                <div class="flex items-center space-x-2 text-xs">
-                    <x-tag class="rounded-md border px-2 py-1">{{ Str::ucfirst($job->experience) }}</x-tag>
-                    <x-tag class="rounded-md border px-2 py-1">{{ $job->category }}</x-tag>
-                </div>
-            </div>
-
-            <p class="text-sm text-slate-500">
-                {!! nl2br(e($job->description)) !!}
-
-            </p>
-        </x-card>
+        </x-job-card>
 
     @endforeach
 </x-layout>
+
+
+
